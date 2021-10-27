deb68df0f34f8b938ac8a4d1103a579f43cb6120
GabrielPradoC <69232929+GabrielPradoC@users.noreply.github.com>
2021-10-27 10:36:55 -0300

    Moved all code compiling to webpack
    Added plugins for handling scss compiling too, now webpack will output all code into the dist folder

--- sprint-1/webpack.config.js
+++ sprint-1/webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: ['./src/index.ts', './src/style.scss'],
   mode: 'production',
   resolve: {
       extensions: ['.ts','.js'],
@@ -12,9 +13,22 @@ module.exports = {
               test: /\.tsx?$/,
               use:'ts-loader',
               exclude: /node_modules/
+          },
+          {
+              test: /\.s[ac]ss$/i,
+              use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                "sass-loader",
+              ]
           }
       ]
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].style.css',
+    }),
+  ],
   output: {
     filename: 'script.js',
     path: path.resolve(__dirname, 'dist'),
