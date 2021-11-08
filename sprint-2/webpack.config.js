db6c22a577a7f6ce91a7580bc50e4b133dac484c
GabrielPradoC <69232929+GabrielPradoC@users.noreply.github.com>
2021-11-08 19:28:42 -0300

    Add babel to config

--- sprint-2/webpack.config.js
+++ sprint-2/webpack.config.js
@@ -7,13 +7,20 @@ module.exports = {
       extensions: ['.ts','.js'],
   },
   module: {
-      rules: [
-          {
-              test: /\.tsx?$/,
-              use:'ts-loader',
-              exclude: /node_modules/
-          }
-      ]
+    rules: [
+      {
+          test: /\.tsx?$/,
+          use:{
+            loader: 'babel-loader',
+            options: {
+              presets: ["@babel/typescript",
+              "@babel/env"],
+              targets: "> 0.25%, not dead"
+            }
+          },
+          exclude: /node_modules/,
+      }
+  ]
   },
   output: {
     filename: 'script.js',
